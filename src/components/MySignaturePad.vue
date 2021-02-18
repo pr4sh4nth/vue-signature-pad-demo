0845b572cb25fa9307feecdf9ef62f723cbd62e1
prashanth <prash488@gmail.com>
2021-02-18 23:36:31 +0800

    Add basic signature pad component and buttons

---
+++ src/components/MySignaturePad.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <VueSignaturePad
+      ref="signaturePad"
+      width="700px"
+      height="500px"
+      class="signaturePad"
+    />
+    <div class="buttonSet">
+      <button @click="save">Save</button>
+      <button @click="undo">Undo</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MySignaturePad",
+  props: {},
+  methods: {
+    undo() {
+      this.$refs.signaturePad.undoSignature();
+    },
+    save() {
+      const { isEmpty, data } = this.$refs.signaturePad.saveSignature();
+      console.log(isEmpty);
+      console.log(data);
+    },
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.signaturePad {
+  margin: 0 auto;
+  border: black 3px solid;
+}
+.buttonSet {
+  max-width: 100%;
+  margin: 10px 0;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueSignaturePad from 'vue-signature-pad';
+
+Vue.use(VueSignaturePad);
 
 Vue.config.productionTip = false
 
